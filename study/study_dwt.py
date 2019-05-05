c1e0cdf798f3c5f57a21fc7d6e942b0297a88f67
lsseong <lsseong@hotmail.com>
2019-05-05 18:40:17 +0800

    Support multiple rounds of denoise in study dwt

--- study/study_dwt.py
+++ study/study_dwt.py
@@ -5,6 +5,26 @@ from statsmodels import robust
 import math
 from study_noise import visualise, summarise
 
+
+def trim_coefficients(wavelet_coeffs):
+    """
+    The time series can be denoised by removing some coefficients from the projections in details.
+    Compute threshold using last level of coefficients
+
+    :param
+        wavelet_coeffs: array of DWT detailed coefficients
+    :return:
+        trimmed coefficients
+    """
+    threshold = compute_threshold(wavelet_coeffs[-1])
+    threshold_coeffs = [None] * len(wavelet_coeffs)
+    for (i, coeffs) in enumerate(wavelet_coeffs):
+        # soft threshold on wavelet coefficients
+        threshold_coeffs[i] = pywt.threshold(coeffs, threshold, mode='hard')
+
+    return threshold_coeffs
+
+
 def compute_threshold(coeffs):
     """
     Compute threshold using VisuShrink approach which employs a single universal threshold to all
@@ -22,13 +42,15 @@ def compute_threshold(coeffs):
     return sigma * math.sqrt(2 * math.log(len(coeffs)))
 
 
-def dwt_denoise(data, label):
+def dwt_denoise(data, label, level=4, rounds=1):
     """
     De-noise a time series using discrete wavelet transform
 
     Args:
         data: Panda series
+        level: A number of decomposition level
         label: A string for data label
+        round: A number indicating rounds of denoising
 
     Returns:
         An array of de-noise series
@@ -37,24 +59,23 @@ def dwt_denoise(data, label):
 
     # The components of the decomposition are divided into the approximation (a) and details (d) at different levels
     # Approximation represents the major feature of the signal and the details describe the detailed changes and noise.
-    wavelet_coeffs = pywt.wavedec(data, 'haar', level=4)
-    np.savetxt("output/dwt/orig_dwt_coeffs_{}.txt".format(label), wavelet_coeffs, fmt='%s')
+    for i in range(rounds):
+        wavelet_coeffs = pywt.wavedec(data, 'haar', level=level)
+        np.savetxt("output/dwt/orig_dwt_coeffs_{}_{}.txt".format(label, i), wavelet_coeffs, fmt='%s')
 
-    # The time series can be denoised by removing some coefficients from the projections in details.
-    # Compute threshold using last level of coefficients
-    threshold = compute_threshold(wavelet_coeffs[-1])
-    threshold_coeffs = [None] * len(wavelet_coeffs)
-    for (i, coeffs) in enumerate(wavelet_coeffs):
-        # soft threshold on wavelet coefficients
-        threshold_coeffs[i] = pywt.threshold(coeffs, threshold, mode='hard')
+        # trim coefficients
+        threshold_coeffs = trim_coefficients(wavelet_coeffs)
 
-    np.savetxt("output/dwt/thresholded_dwt_coeffs_{}.txt".format(label), threshold_coeffs, fmt='%s')
+        # save
+        np.savetxt("output/dwt/thresholded_dwt_coeffs_{}_{}.txt".format(label, i), threshold_coeffs, fmt='%s')
 
-    # reconstruct data using thresholded coefficients
-    denoised_data = pywt.waverec(threshold_coeffs, 'haar')
-    np.savetxt("output/dwt/denoise_{}.txt".format(label), denoised_data, fmt='%s')
+        # reconstruct data using thresholded coefficients
+        denoised_data = pywt.waverec(threshold_coeffs, 'haar')
+        np.savetxt("output/dwt/denoise_{}_{}.txt".format(label, i), denoised_data, fmt='%s')
 
-    return denoised_data
+        data = pd.Series(denoised_data, index=data.index)
+
+    return data
 
 
 def study_JPM():
@@ -72,10 +93,10 @@ def study_HSI():
 if __name__ == "__main__":
     df = study_HSI()
 
-    df['open_denoise'] = pd.Series(dwt_denoise(df["open"], "open"), index=df.index)
-    df['high_denoise'] = pd.Series(dwt_denoise(df["high"], "high"), index=df.index)
-    df['low_denoise'] = pd.Series(dwt_denoise(df["low"], "low"), index=df.index)
-    df['close_denoise'] = pd.Series(dwt_denoise(df["close"], "close"), index=df.index)
+    df['open_denoise'] = pd.Series(dwt_denoise(df["open"], level=2, label="open", rounds=2), index=df.index)
+    df['high_denoise'] = pd.Series(dwt_denoise(df["high"], level=2, label="high", rounds=2), index=df.index)
+    df['low_denoise'] = pd.Series(dwt_denoise(df["low"], level=2, label="low", rounds=2), index=df.index)
+    df['close_denoise'] = pd.Series(dwt_denoise(df["close"], level=2, label="close", rounds=2), index=df.index)
 
     df.to_csv('output/dwt/dwt_output.csv', sep=',')
 
